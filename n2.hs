f251eda11ab0723821d53dba1e676c5f54139a59
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-06-28 11:30:45 -0300

    commit

---
+++ n2.hs
@@ -0,0 +1,16 @@
+analiseaux1 :: String -> Char -> Int
+analiseaux1 [] n = 0
+analiseaux1 listas n = length [y | y <- listas, y == n]
+analise :: String -> [(Char, Int)]
+analise lista = [(x,y) | x <- lista, y <- [analiseaux1 lista x]]
+
+quicksortupla :: [(Char,Int)] -> [(Char,Int)]
+quicksortupla [] = []
+quicksortupla (x:xs) = quicksortupla [y | y <- xs , snd y <= snd x] ++ [x] ++ quicksortupla [y | y <- xs , snd y > snd x]
+
+ordefreqaux :: String -> [(Char,Int)]
+ordefreqaux [] = []
+ordefreqaux listt = reverse (quicksortupla (analise listt))
+ordefreq :: String -> String
+ordefreq [] = []
+ordefreq list2 = [x | (x,_) <- ordefreqaux list2]
