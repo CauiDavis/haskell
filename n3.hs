53a7b4293186d5b2d0e640ebbb6538a17cce1987
cauidavismatosdeoliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2022-07-16 11:58:06 -0300

    Update n3.hs

--- n3.hs
+++ n3.hs
@@ -1,4 +1,5 @@
-type Produto = (String,Int,Float)
+
+produtos = [("coxinha",3,345.10),("bomba",2,250.5),("pastel",4,423.10)]
 data Arvore a = Nulo | No (Arvore a) a (Arvore a)
                deriving (Show)
 criar_arvore :: (Ord a) => [a] -> Arvore a
@@ -13,3 +14,17 @@ inserir Nulo x = No Nulo x Nulo
 inserir (No arv1 v arv2) x | (v == x) = No arv1 v arv2
                            | (v < x) = No arv1 v (inserir arv2 x)
                            | (v > x) = No (inserir arv1 x) v arv2
+
+buscar :: (Ord a) => [a] -> a -> [a]
+buscar [] n = []
+buscar listb n = [x | x <- listb, x == n]
+
+buscararvore ::(Ord a) => [a] -> a -> Arvore a
+buscararvore listba m  = criar_arvore (buscar listba m)
+
+atualizar :: (Ord a) => [a] -> [a] -> Arvore a
+atualizar n m = criar_arvore (n ++ m)
+
+total :: [(String,Integer,Double)] -> String
+total [] = "R$:0.0"
+total listt = "R$:" ++ show (sum [x | (_,_,x) <- listt])
